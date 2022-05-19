852d12d4e24785ab00fc6b0004e30175272bd166
Jacob  Bauer <jacobbauer@Jacobs-MacBook-Air.local>
2022-05-19 16:01:05 -0500

    tested player stats

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -1,15 +1,35 @@
 const Potion = require('../lib/Potion');
+const Player = require('../lib/Player');
 
 jest.mock('../lib/Potion');
 
-const Player = require('../lib/Player');
 
-test(' creates a player object', () => {
+test('creates a player object', () => {
     const player = new Player('Jake');
 
     expect(player.name).toBe('Jake');
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
-}); 
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Jake');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Jake');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
